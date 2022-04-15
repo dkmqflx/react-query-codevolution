e8d275f9969b1ad6571b28e9aa9d85e8d597b03f
dkmqflx <dkmqflx@gmail.com>
2022-04-15 16:32:45 +0900

    19. Paginated Queries

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { RQSuperHeroPage } from './components/RQSuperHero.page';
 import { ParallelQueriesPage } from './components/ParallelQueries.page';
 import { DynamicParallelPage } from './components/DynamicParallel.page';
 import { DependentQueriesPage } from './components/DependentQueries.page';
+import { PaginatedQueriesPage } from './components/PaginatedQueries.page';
 
 const queryClient = new QueryClient();
 
@@ -30,6 +31,10 @@ function App() {
             </ul>
           </nav>
           <Switch>
+            <Route path="/rq-paginated">
+              <PaginatedQueriesPage />
+            </Route>
+
             <Route path="/rq-dependent">
               <DependentQueriesPage email="vishwas@example.com" />
             </Route>
