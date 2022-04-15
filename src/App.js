2d093803d6bd088e6a5fe8bb5f4944656d1d788f
dkmqflx <dkmqflx@gmail.com>
2022-04-15 10:47:04 +0900

    3. Fetching Data with useQuery

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import { HomePage } from './components/Home.page';
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page';
 import { SuperHeroesPage } from './components/SuperHeroes.page';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/super-heroes">
-            <SuperHeroesPage />
-          </Route>
-          <Route path="/rq-super-heroes" component={RQSuperHeroesPage}></Route>
-          <Route path="/" component={HomePage}></Route>
-        </Switch>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/super-heroes">
+              <SuperHeroesPage />
+            </Route>
+            <Route path="/rq-super-heroes" component={RQSuperHeroesPage}></Route>
+            <Route path="/" component={HomePage}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
