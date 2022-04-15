113dd133338d3224ad814075351ba11bee4e5a27
dkmqflx <dkmqflx@gmail.com>
2022-04-15 12:21:11 +0900

    11. Success and Error Callbacks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,19 +6,25 @@ const fetchSupserHeroes = () => {
 };
 
 /*
-  다시 마운트가 되지 않더라도 유저가 하는 이벤트를 통해서 데이터를 다시 fetch할 수 있다.
-  enabled: false, 속성을 통해 데이터가 fetch되는 것을 막을 수 있다.
-  그리고 useQuery의 refetch를 사용해서 이벤트가 발생할 때 마다 refetch할 수 있도록 한다.
-  그리고 처음에는 isLoading이 true지만 다시데이터를 눌를 때는 캐시되기 때문에 isLoading이 false가 된다.
-  즉, iseFetching만 true -> false로 바뀐다.
+data fetching을 다루면서 side effect 발생하고 싶을 때 있다.
+예를들어 모달창을 연다던가, 다른 라우트로 이동하거나 toast notification을 보여주는 것.
 
  */
 
 export const RQSuperHeroesPage = () => {
+  const onSuccess = (data) => {
+    console.log('Perform side deffect after data fetching', data);
+  };
+
+  const onError = (error) => {
+    console.log('Perform side deffect after encountering error', error);
+  };
+
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery('super-heroes', fetchSupserHeroes, {
-    enabled: false,
+    onSuccess: onSuccess,
+    onError: onError,
   });
-  console.log(isLoading, isFetching);
+  console.log({ isLoading, isFetching });
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
