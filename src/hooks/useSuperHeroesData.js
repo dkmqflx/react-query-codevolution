0e86f96a56fc8d260dc56a1d2bd05b445ddc50e9
dkmqflx <dkmqflx@gmail.com>
2022-04-15 23:21:52 +0900

    25. Axios Interceptor

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,8 +1,9 @@
 import { useQuery, useMutation, useQueryClient } from 'react-query';
-import axios from 'axios';
+import { request } from '../utils/axios-utils';
 
 const fetchSupserHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes');
+  // return axios.get('http://localhost:4000/superheroes');
+  return request({ url: '/superheroes' });
 };
 
 export const useSuperHeroesData = (onSuccess, onError) => {
@@ -17,7 +18,8 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 };
 
 const addSuperHero = (hero) => {
-  return axios.post('http://localhost:4000/superheroes', hero);
+  // return axios.post('http://localhost:4000/superheroes', hero);
+  return request({ url: '/superheroes', method: 'post', data: hero });
 };
 
 export const useAddSuperHeroData = () => {
